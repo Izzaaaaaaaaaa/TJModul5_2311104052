2cc337db33065fc3e734a1e99ccf33eac28db390
izzaaaaaaaaaa <izzatyzahara2424@gmail.com>
2025-03-20 20:38:53 +0700

    Menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Penjumlahan
+{
+    public static T JumlahTigaAngka<T>(T a, T b, T c)
+    {
+        dynamic x = a;
+        dynamic y = b;
+        dynamic z = c;
+        return x + y + z;
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        // Ganti dengan dua digit dari NIM
+        int angka1 = 10;
+        int angka2 = 40;
+        int angka3 = 52;
+
+        // Tentukan tipe data berdasarkan digit terakhir NIM
+        var hasil = Penjumlahan.JumlahTigaAngka(angka1, angka2, angka3);
+
+        Console.WriteLine($"Hasil Penjumlahan: {hasil}");
+        Console.ReadLine();
+    }
+}
